6feb065466bd9d9fc8edeb6d05288ddcb8ce2f0b
Ivan Salinas <ivan.salinas1991@gmail.com>
2018-06-05 20:33:29 -0700

    Added movement and head tracking.

---
+++ FPS_Game/Assets/Scripts/FPSController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(CharacterController))]
+public class FPSController : MonoBehaviour {
+
+  public float movementSpeed = 10.0f;
+  public float rotationSensitivity = 5.0f;
+
+  float pitch = 0;
+  float yaw = 0;
+  public float pitchRange = 60.0f;
+
+  float verticalVelocity = 0;
+  float jumpSpeed = 5.0f;
+
+  CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+		Screen.lockCursor = true;
+    characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    // Key-driven Rotation
+    yaw += Input.GetAxis("Mouse X") * rotationSensitivity;
+    pitch += Input.GetAxis("Mouse Y") * rotationSensitivity;
+    pitch = Mathf.Clamp(pitch, -pitchRange, pitchRange);
+    if (yaw != 0 || pitch != 0) {
+      Camera.main.transform.localRotation *= Quaternion.Euler(pitch, yaw, 0);
+      yaw = 0;
+      pitch = 0;
+    }
+
+    // Jumping
+    verticalVelocity += Physics.gravity.y * Time.deltaTime;
+    if (characterController.isGrounded && Input.GetButtonDown("Jump")) {
+      verticalVelocity = jumpSpeed;
+    }
+
+    // Movement
+    float forwardSpeed = Input.GetAxis("Vertical") * movementSpeed;
+		float lateralSpeed = Input.GetAxis("Horizontal") * movementSpeed;
+
+    Vector3 speed = new Vector3(lateralSpeed, verticalVelocity, forwardSpeed);
+    Quaternion q = new Quaternion(0, Camera.main.transform.rotation.y, 0, Camera.main.transform.rotation.w);
+    speed = q * speed;
+
+    characterController.Move(speed * Time.deltaTime);
+	}
+}
---
+++ FPS_Game/Assets/Scripts/ReadUSB.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+
+public class ReadUSB : MonoBehaviour {
+
+  const int baudrate = 115200;
+
+  // Specify Correct Port Name
+  const string portName = "/dev/cu.usbmodem2822651";
+
+  SerialPort serialPort = new SerialPort(portName, baudrate);
+
+  void Start () {
+    serialPort.ReadTimeout = 100;
+    serialPort.Open();
+
+    if( !serialPort.IsOpen ) {
+
+      Debug.LogError("Couldn't open " + portName);
+
+    }
+
+  }
+    
+  void Update () {
+
+    List<byte> buffer = new List<byte>();
+
+    // Reading 1024 bytes
+    for (int i = 0; i < 64; i++)
+    {
+      buffer.Add((byte)serialPort.ReadByte());
+    }
+
+    // Convert list of bytes to string
+    string te = System.Text.Encoding.UTF8.GetString(buffer.ToArray());
+    //Debug.Log(te);
+
+    // Split string into lines
+    string[] lines = te.Split( '\n' );
+
+    if ( lines.Length >= 2) {
+
+      // Split line by spaces
+      string[] line = lines[lines.Length - 2].Split( ' ' );
+
+      // If the line starts with "QC", parse the Quaternion
+      if ( line[0] == "QC" && line.Length >= 5 ) {
+        
+        Quaternion q = new Quaternion( float.Parse(line[2]),
+          float.Parse(line[3]),
+          -float.Parse(line[4]),
+          float.Parse(line[1]));
+
+        transform.rotation = Quaternion.Inverse(q);
+
+      }
+
+    }
+
+  }
+
+
+  void OnGUI()
+  {
+    string euler = "Euler angle: " + transform.eulerAngles.x + ", " +
+      transform.eulerAngles.y + ", " + transform.eulerAngles.z;
+  }
+}
+
